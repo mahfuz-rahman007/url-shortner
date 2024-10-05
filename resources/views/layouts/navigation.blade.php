fb4f72a591ca0b1a776fd367e64b5e7f8bf20ea6
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-10-05 19:58:48 +0600

    list and create link added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,18 +10,17 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div>
-
-
                 <!-- Url Shortener List Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('url.list')" :active="request()->routeIs('url.list')">
                         {{ __('Shortened Url List') }}
+                    </x-nav-link>
+                </div>
+
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="'/'" :active="false">
+                        {{ __('Create Shortened Url') }}
                     </x-nav-link>
                 </div>
             </div>
