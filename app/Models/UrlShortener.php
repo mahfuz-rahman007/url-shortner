c6d45da14c8303e2adc92ff9ffca1907b986136f
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-11-05 09:23:00 +0600

    user relation added

--- app/Models/UrlShortener.php
+++ app/Models/UrlShortener.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UrlShortener extends Model
 {
@@ -16,14 +17,22 @@ class UrlShortener extends Model
     ];
 
     /**
- * Get the attributes that should be cast.
- *
- * @return array<string, string>
- */
-protected function casts(): array
-{
-    return [
-        'last_clicked_at' => 'datetime:Y-m-d'
-    ];
-}
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'last_clicked_at' => 'datetime:Y-m-d'
+        ];
+    }
+
+    /**
+     * Get the user that owns the URL.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
