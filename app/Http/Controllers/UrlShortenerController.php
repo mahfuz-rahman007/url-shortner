2cd73a4d9dfa1348448a10b20a8eb0bfb9c6df4a
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2024-11-05 09:26:12 +0600

    user remove from query

--- app/Http/Controllers/UrlShortenerController.php
+++ app/Http/Controllers/UrlShortenerController.php
@@ -26,7 +26,7 @@ class UrlShortenerController extends Controller
      */
     public function allShortenerUrlList()
     {
-        $urls = UrlShortener::with('user')->where('user_id', '!=', Auth::user()->id)->orderBy('created_at', 'desc')->paginate(15);
+        $urls = UrlShortener::where('user_id', '!=', Auth::user()->id)->orderBy('created_at', 'desc')->paginate(15);
         return view('shortener-url.all-list', compact('urls'));
     }
 
