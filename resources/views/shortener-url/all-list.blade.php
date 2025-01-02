2c18202e5b492a58a838d618a693e73fd833ec06
mahfuz-rahman007 <mahfujkhan017@gmail.com>
2025-01-02 23:39:30 +0600

    table css modified

--- resources/views/shortener-url/all-list.blade.php
+++ resources/views/shortener-url/all-list.blade.php
@@ -19,55 +19,49 @@
                                     <table class="table-auto min-w-full divide-y divide-gray-200">
                                         <thead>
                                             <tr>
-                                                <th scope="col"
-                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                <th class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
                                                     No.</th>
-                                                <th scope="col"
-                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                <th class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
                                                     User</th>
-                                                <th scope="col"
-                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                                <th class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
                                                     New Url</th>
-                                                <th scope="col"
-                                                    class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase max-w-md">
+                                                <th class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase max-w-[28rem]">
                                                     Old Url</th>
-                                                <th scope="col"
-                                                    class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase">
+                                                <th class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase">
                                                     Clicks</th>
-                                                <th scope="col"
-                                                    class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase">
+                                                <th class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase">
                                                     Last Visited</th>
                                                     <th scope="col"
-                                                    class="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase">
+                                                    class="px-4 py-3 text-center text-xs font-medium text-gray-500 uppercase">
                                                     Created At</th>
-                                                <th scope="col"
-                                                    class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">
+                                                <th class="px-4 py-3 text-end text-xs font-medium text-gray-500 uppercase">
                                                     Action</th>
                                             </tr>
                                         </thead>
                                         <tbody class="divide-y divide-gray-200">
+                                            
                                             @foreach ($urls as $url)
                                                 <tr
                                                     class="odd:bg-white even:bg-gray-100 hover:bg-gray-100">
-                                                    <td class="px-6 py-4 text-sm">
+                                                    <td class="px-4 py-3 text-xs">
                                                         {{ ($urls->currentpage() - 1) * $urls->perpage() + $loop->iteration }}
                                                     </td>
-                                                    <td class="px-6 py-4 text-sm">
+                                                    <td class="px-4 py-3 text-xs">
                                                         {{ $url->user ? $url->user->name : 'Unknown' }}
                                                     </td>
-                                                    <td class="px-6 py-4 text-sm">
+                                                    <td class="px-4 py-3 text-xs">
                                                         <a href="{{ url('/') . '/' . $url->slug }}" target="_blank"
                                                             rel="noopener noreferrer">{{ url('/') . '/' . $url->slug }}</a>
                                                     </td>
-                                                    <td class="px-6 py-4 text-sm max-w-md break-words">{{ $url->original_url }}</td>
-                                                    <td class="px-6 py-4 text-sm text-center">{{ $url->clicks }}</td>
-                                                    <td class="px-6 py-4 text-sm text-center">
+                                                    <td class="px-4 py-3 text-xs max-w-[20rem] break-words">{{ $url->original_url }}</td>
+                                                    <td class="px-4 py-3 text-xs text-center">{{ $url->clicks }}</td>
+                                                    <td class="px-4 py-3 text-xs text-center">
                                                         {{ $url->last_clicked_at ? $url->last_clicked_at->diffForHumans() : 'Not Visited' }}
                                                     </td>
-                                                    <td class="px-6 py-4 text-sm">
-                                                        {{ $url->created_at }}
+                                                    <td class="px-4 py-3 text-xs">
+                                                        {{ $url->created_at->format('d-m-y h:i a') }}
                                                     </td>
-                                                    <td class="px-6 py-4 text-sm">
+                                                    <td class="px-4 py-3 text-xs">
                                                         {{-- Link edit and delete button --}}
                                                         <div class="flex justify-end">
                                                             <div class="flex gap-2">
@@ -86,7 +80,7 @@
                                         </tbody>
                                     </table>
 
-                                    <div class="mt-4 mr-3 ml-3">
+                                    <div class="mt-5 mb-5 mr-3 ml-3">
                                         {{ $urls->onEachSide(1)->links() }}
                                     </div>
                                 </div>
